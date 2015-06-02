213224240b29c6e20a51406b9a373529786d7b13
Sebastia at W500 <sebastiasebas@gmail.com>
2015-06-02 22:37:16 +0200

    v 1.0.b - open() queue)

--- mqunit.dfm
+++ mqunit.dfm
@@ -2,35 +2,43 @@ object fMQ: TfMQ
   Left = 0
   Top = 0
   Caption = 'MQ API'
-  ClientHeight = 356
-  ClientWidth = 1184
+  ClientHeight = 677
+  ClientWidth = 1548
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
-  Font.Height = -11
+  Font.Height = -14
   Font.Name = 'Tahoma'
   Font.Style = []
   OldCreateOrder = False
   OnCreate = MyInit
-  PixelsPerInch = 96
-  TextHeight = 13
+  PixelsPerInch = 120
+  TextHeight = 17
   object btnConnect: TButton
-    Left = 256
-    Top = 17
-    Width = 233
-    Height = 33
+    Left = 335
+    Top = 15
+    Width = 304
+    Height = 35
+    Margins.Left = 4
+    Margins.Top = 4
+    Margins.Right = 4
+    Margins.Bottom = 4
     Caption = 'Connect to Qmgr'
     TabOrder = 0
     OnClick = btnConnectClick
   end
   object edQMN: TEdit
     Left = 32
-    Top = 16
-    Width = 209
-    Height = 31
+    Top = 9
+    Width = 273
+    Height = 36
+    Margins.Left = 4
+    Margins.Top = 4
+    Margins.Right = 4
+    Margins.Bottom = 4
     Font.Charset = DEFAULT_CHARSET
     Font.Color = clWindowText
-    Font.Height = -19
+    Font.Height = -23
     Font.Name = 'Tahoma'
     Font.Style = []
     ParentFont = False
@@ -38,13 +46,17 @@ object fMQ: TfMQ
     Text = 'Queue Manager Name'
   end
   object edCH: TEdit
-    Left = 504
-    Top = 16
-    Width = 169
-    Height = 31
+    Left = 659
+    Top = 15
+    Width = 221
+    Height = 36
+    Margins.Left = 4
+    Margins.Top = 4
+    Margins.Right = 4
+    Margins.Bottom = 4
     Font.Charset = DEFAULT_CHARSET
     Font.Color = clWindowText
-    Font.Height = -19
+    Font.Height = -23
     Font.Name = 'Tahoma'
     Font.Style = []
     ParentFont = False
@@ -53,20 +65,61 @@ object fMQ: TfMQ
   end
   object lbEvents: TListBox
     Left = 32
-    Top = 64
-    Width = 1129
-    Height = 257
+    Top = 324
+    Width = 1486
+    Height = 336
+    Margins.Left = 4
+    Margins.Top = 4
+    Margins.Right = 4
+    Margins.Bottom = 4
     Font.Charset = ANSI_CHARSET
     Font.Color = clWindowText
-    Font.Height = -11
+    Font.Height = -15
     Font.Name = 'Lucida Console'
     Font.Style = []
-    ItemHeight = 11
+    ItemHeight = 15
     ParentFont = False
     TabOrder = 3
   end
+  object edQUNM: TEdit
+    Left = 32
+    Top = 75
+    Width = 273
+    Height = 36
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -23
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 4
+    Text = 'Queue Name'
+  end
+  object btnOpen: TButton
+    Left = 335
+    Top = 80
+    Width = 305
+    Height = 33
+    Caption = 'Open Queue'
+    TabOrder = 5
+    OnClick = btnOpenClick
+  end
+  object edObjH: TEdit
+    Left = 656
+    Top = 80
+    Width = 225
+    Height = 36
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -23
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 6
+    Text = 'Object Handle'
+  end
   object Timer1: TTimer
-    Left = 688
+    Left = 920
     Top = 16
   end
 end
--- mqunit.pas
+++ mqunit.pas
@@ -6,6 +6,10 @@ unit mqunit;
  * MQIS -> Server libraries + mq_basic
  * MQIC -> Client libraries + mq_basicC
  *
+ * Versions :
+ *  1.0.a - 20150520 - problemes doble char
+ *  1.0.b - 20150602 - open() queue
+ *
  *}
 
 interface
@@ -19,6 +23,12 @@ uses
 //  mq_basicC,
   SAG_debug ;
 
+const
+  szProducte = 'MQ API access from Delphi' ;
+  szAutor    = 'sag@tinet.cat' ;
+  szVersio   = 'v 1.0.b, 20150602' ;
+
+
 type
   TfMQ = class(TForm)
     btnConnect: TButton;
@@ -26,8 +36,12 @@ type
     edCH: TEdit;
     lbEvents: TListBox;
     Timer1: TTimer;
+    edQUNM: TEdit;
+    btnOpen: TButton;
+    edObjH: TEdit;
     procedure MyInit(Sender: TObject);
     procedure btnConnectClick(Sender: TObject);
+    procedure btnOpenClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -43,9 +57,14 @@ type
 
 var
   fMQ         : TfMQ;
-  szQMN       : MQCHAR48 ; // queue manager name
-  hConexio    : THandle ;
+
+  sz48QMN     : MQCHAR48 ;       // queue manager name
+  hConexio    : THandle ;        // Connect() handle
   szhConexio  : string ;
+
+  sz48QUEUENM : MQCHAR48 ;       // queue name
+  hOpen       : THandle ;        // Open() handle
+  szhOpen     : string ;
 
 implementation
 
@@ -68,14 +87,14 @@ begin
 //  Hex_Dump ( fMQ.lbEvents, 'QMnameUser', @ szUsrQMN, sizeof (szUsrQMN) ) ;
 
 //  szQMN :=  '                                                ' ; // init 48byte array
-  strpcopy( szQMN, szUsrQMN ) ;
-  Hex_Dump ( fMQ.lbEvents, 'QMname', @ szQMN, sizeof (szQMN) ) ;
+  strpcopy( sz48QMN, szUsrQMN ) ;
+  Hex_Dump ( fMQ.lbEvents, 'QMname', @ sz48QMN, sizeof (sz48QMN) ) ;
 
-  iLng := sizeof( szQMN ) ;
-  SAGdebugMsg ( fMQ.lbEvents, '>>> Lets connect to QMGR ['+IntToStr(iLng)+']-('+ szQMN +').' ) ;
+  iLng := sizeof( sz48QMN ) ;
+  SAGdebugMsg ( fMQ.lbEvents, '>>> Lets connect to QMGR [lng '+IntToStr(iLng)+']-('+ sz48QMN +').' ) ;
 
-  hConexio := ConnectMQ ( fMQ.lbEvents, szQMN ) ;
-  szhConexio := IntToStr(hConexio) ;
+  hConexio := ConnectMQ ( fMQ.lbEvents, sz48QMN ) ;
+  szhConexio := IntToStr( hConexio ) ;
   edCH.Text := szhConexio ;
 
   SAGdebugMsg ( fMQ.lbEvents, '+-+-+- Resultat handleConexio(' + szhConexio + ').' ) ;
@@ -89,6 +108,40 @@ begin
 end; // TfMQ.btnConnectClick
 
 
+procedure TfMQ.btnOpenClick(Sender: TObject);
+var
+  szUsrQueueName : AnsiString ;
+  iLng           : integer ;
+  oo             : integer ;  // Open Options
+  od             : MQOD ;     // Object Descriptor
+
+begin
+
+  szUsrQueueName := edQUNM.Text ;
+  strpcopy( sz48QUEUENM, szUsrQueueName ) ;
+
+  iLng := sizeof( sz48QUEUENM ) ;
+  SAGdebugMsg ( fMQ.lbEvents, '>>> Lets OPEN() Queue [lng '+IntToStr(iLng)+']-('+ sz48QUEUENM +').' ) ;
+
+  oo := MQOO_INPUT_SHARED + MQOO_FAIL_IF_QUIESCING ;
+
+  // init Object Descriptor fields with default values, then set few values
+  move ( MQOD_DEFAULT, od, sizeof (MQOD_DEFAULT) ) ;  // source, destination, count.
+  od.Version := MQOD_VERSION_3 ;
+
+  strplcopy ( od.ObjectQMgrName, sz48QMN, MQ_Q_MGR_NAME_LENGTH ) ;
+  strplcopy ( od.ObjectName, sz48QUEUENM, sizeof(od.ObjectName)-1 ) ;
+
+  hOpen := MQ_Open_Queue ( fMQ.lbEvents, hConexio, sz48QUEUENM, od, oo ) ; // hOpen = 0 if error
+
+  szhOpen := IntToStr( hOpen ) ;
+  edObjH.Text := szhOpen ;
+
+  SAGdebugMsg ( fMQ.lbEvents, '+-+-+- Resultat handleOpen(' + szhOpen + ').' ) ;
+
+end; // TfMQ.btnOpenClick
+
+
 procedure TfMQ.MyInit(Sender: TObject);
 begin
   edQMN.Text := 'QMV75' ;
@@ -96,10 +149,14 @@ begin
   edQMN.Text := 'SEBAS.SVRCONN/TCP/9.137.166.87(2415)' ; // P7029 at Linux - v 7.5
   edQMN.Text := 'QM_CNT' ;                               // T430
   edQMN.Text := 'QMU' ;                                  // T440
+  edQMN.Text := 'QM_CNT' ;                               // W500
   edCH.Text := '-' ;
+
+  edQUNM.Text := 'QL.IN' ;
+  edObjH.Text := '-' ;
 
   Timer1.Enabled := false ;
   hConexio := 0 ;
-end;
+end; // OnCreate() -> TfMQ.MyInit
 
 end.
